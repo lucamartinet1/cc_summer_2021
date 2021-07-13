04f8f765964e677d5a8bf7155c9d0c76313fd5bb
Luca <73172807+lucamartinet1@users.noreply.github.com>
2021-07-13 11:11:07 +0200

    Update main.rb

--- Feu - Participants/Bélier - Participant/main.rb
+++ Feu - Participants/Bélier - Participant/main.rb
@@ -1,18 +1,14 @@
 require './verification'
-allrubies = [] 
-File.open("rubies.txt", "w+")
-    puts(allrubies)
-  end
+allrubies = [] << allrubies = CSV.parse(File.read(rubies.txt), headers: true)
+File.open(rubies.txt, w+)
+file = open(file.txt, r)
 
 > "ruby" 
 
 def getrubies:
     line_num=0
-text=File.open('rubies.txt').read
-text.gsub!(/\r\n?/, "\n")
+text=File.open(rubies.txt).read
+text.gsub!(/\r\n?/, \n)
 text.each_line do |line|
     print "#{line_num += 1} #{line}"
 end
-
-
-
